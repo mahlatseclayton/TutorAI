5c4a73ec7dd1858eae6bd1f52c834b354fd83cc4
mahlatseclayton <mahlatseclayton1@gmail.com>
2026-02-16 22:36:27 +0200

    firebase setup completed

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDjNyRSkepOLHcYuK4ALI2xWibC-P849f0",
+  authDomain: "tutorai-5f97d.firebaseapp.com",
+  projectId: "tutorai-5f97d",
+  storageBucket: "tutorai-5f97d.firebasestorage.app",
+  messagingSenderId: "447039931727",
+  appId: "1:447039931727:web:e90fa57709aced5bb6713a",
+  measurementId: "G-TDDTB6PM28"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+function signUp(){
+    
+}
