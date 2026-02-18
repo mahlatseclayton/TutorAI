daa9f57ae8f5639e77c7b6df1cf160cec9fd65e2
mahlatseclayton <mahlatseclayton1@gmail.com>
2026-02-19 00:38:14 +0200

    firebase intergration,signin,signup and forgot password

--- firebase.js
+++ firebase.js
@@ -4,6 +4,19 @@
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, doc, setDoc } 
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth, 
+         createUserWithEmailAndPassword, 
+         signInWithEmailAndPassword, 
+         sendPasswordResetEmail,
+         onAuthStateChanged, 
+         updateProfile,
+         sendEmailVerification, 
+         signOut 
+       } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 const firebaseConfig = {
   apiKey: "AIzaSyDjNyRSkepOLHcYuK4ALI2xWibC-P849f0",
   authDomain: "tutorai-5f97d.firebaseapp.com",
@@ -13,22 +26,94 @@ const firebaseConfig = {
   appId: "1:447039931727:web:e90fa57709aced5bb6713a",
   measurementId: "G-TDDTB6PM28"
 };
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-function signUp(){
-    let name=document.getElementById("name").value;
-    let grade=document.getElementById("grade").value;
-    let email=document.getElementById("email").value;
-    let password=document.getElementById("password").value;
-    let cpassword=document.getElementById("cpassword").value;
+// the export allows us to use the method outside this file and the async to allow the program to wait a bit
+async function signUp(){
+    const name=document.getElementById("name").value;
+    const grade=document.getElementById("grade").value;
+    const email=document.getElementById("email").value;
+    const password=document.getElementById("password").value;
+    const cpassword=document.getElementById("cpassword").value;
+   
     if(password!=cpassword){
         alert("Passwords do not match");
     //   validation
     }
+    else if(password.length<6){
+        alert("Password must be at least 6 characters.");
+    }
+    // add other validations here for security purposes
     else{
     //   signup here
+    try{
+        // create a user credetials then send verification using the user object??
+       const userCredential= await createUserWithEmailAndPassword(auth,email,password);
+       const user=userCredential.user;
 
+    //    pause verifications for now
+        // await sendEmailVerification(user);
+     await   setDoc(doc(db, "users", user.uid), {
+    name: name,
+    grade: grade,
+    email: email,
+    createdAt: new Date()
+});
+        alert("Success:Account successfully created!");
+        
+        window.location.href="signIn.html";
+       
     }
+    catch(error){
+        alert("Error creating account :",error.message);
+        
+    } 
+    
+    }
+
+}
+    //  make an oncliick lister for the signup button
+    const signUpbtn=document.getElementById("signUpBtn");
+if (signUpbtn) {
+    signUpbtn.addEventListener("click", signUp);
+}
+const signInBtn=document.getElementById("signInBtn");
+if(signInBtn){
+    
+    signInBtn.addEventListener("click",async () =>{
+         const email=document.getElementById("email").value;
+    const password=document.getElementById("password").value;
+        try{
+               const userCredential= await signInWithEmailAndPassword(auth,email,password);
+                const user=userCredential.user;
+                // checking verification
+                // if(!user.emailVerified){
+                //     alert("Verify email to log in.")
+                //     auth.signOut;
+                //     return;
+                // }
+                alert("Success:Log in successful.");
+                     window.location.href="mainPage.html";
+        }catch(error){
+                alert("Error:Log in failed , validate your email or password .");
+                console.log(error.message);
+                
+        }
+
+    })
+}
+// forgot password funtionality
+const forgotBtn=document.getElementById("forgotBtn");
+if(forgotBtn){
+
+    forgotBtn.addEventListener("click",async()=>{
+         const email=document.getElementById("email").value;
+        await sendPasswordResetEmail(auth,email);
+        alert("Reset password link sent.");
+    })
 }
